635e84720e02f418423737bafc5d72d3f104f236
Shaunik Musukula <68877932+shaunikm@users.noreply.github.com>
2023-10-26 23:45:34 -0700

    update target & cloud settings

--- settings.py
+++ settings.py
@@ -8,12 +8,24 @@ import random
 WIDTH = 1024
 HEIGHT = 768
 FPS = 30
+SCORE_INC = 1/60
+
+# entity settings
+CLOUD_COUNT = 7
+TARGET_COUNT = 3
+CLOUD_SPEED = 4
+TARGET_SPEED = 15
+SPEED_FAC = 2
 
 # player settings
 PLAYER_JUMP = 25
 PLAYER_GRAV = 1.5
 global PLAYER_FRIC
 PLAYER_FRIC = 0.2
+
+# text settings
+FONT_SIZE = 22
+SPACING = 20
 
 # define colors
 WHITE = (255, 255, 255)
@@ -26,4 +38,7 @@ D62628 = (214, 40, 40)
 FCBF49 = (252, 191, 73)
 F77F00 = (247, 127, 0)
 
-CLOUD_LIST = [(60, 40), (50, 40), (30, 20)]
+CLOUD_LIST = ["serene.png",
+              "disc.png",
+              "furious.png"]
+CLOUD_WEIGHTS = (60, 10, 3)
